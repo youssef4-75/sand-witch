bde0daed519eaa011b878ae19a8fce2ba78e74f8
haddadi <youssefhaddadi115@gmail.com>
2025-01-03 12:02:03 +0100

    the copyright

--- script/popUp/listMode.js
+++ script/popUp/listMode.js
@@ -4,7 +4,7 @@ let listMode = [
     new Mode(`Hasbulla Burger`, `mode_images/Hasbulla.jpeg`, `made by OTHMANE WINA`, `#f5e42a`, ``, `https://othmane-wina.github.io/sandwich-maker/`, `background: linear-gradient(to left, black, rgb(3, 18, 56));`),
     new Mode(`krabbypattymaker`, `mode_images/krabby_patty.jpg`, `made by Ertil Riyad`, `#08284b`, ``, `https://krabbypattymaker.vercel.app/HomePage.html`),
     new Mode(`Flapping Sandwitch`, `mode_images/flying_burger.jpeg`, `made by Yassine Lamsaaf`, `green`, ``, `https://yassinelamsaaf.github.io/SandwichMaker/flappySandwich/index.html`),
-    new Mode(`sandwich sprint`, `mode_images/sandwich_sprint.jpg`, `made by Mohammed Mehdi Bahri`, `#23C7E0`, ``, `https://itsmhido.github.io/sandwich-sprint/`),
+    new Mode(`sandwich sprint`, `mode_images/sandwich_sprint1.jpg`, `made by Mohammed Mehdi Bahri`, `#23C7E0`, ``, `https://itsmhido.github.io/sandwich-sprint/`),
     new Mode(`burger train`, `mode_images/sandwich_train.jpg`, `made by Mohsine Chkirida`, `#F0E68C`, ``, `https://mohssine-chkirida.github.io/Sandwich-Maker/`),
 
 ]
